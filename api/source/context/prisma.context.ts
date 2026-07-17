0c6f931d75813523d8d2c8e762c3221da8cba73b
roney ruiz <roneyruizrojas@gmail.com>
2026-07-16 21:40:25 -0600

    chore(app): purge complete api server completly

--- api/source/context/prisma.context.ts
+++
@@ -1,8 +0,0 @@
-import { env } from "@env";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/generated/client";
-
-const connectionString = env.POSTGRES_URL;
-
-const adapter = new PrismaPg({ connectionString });
-export const prisma = new PrismaClient({ adapter });
--- api/source/plugins/health.plugin.ts
+++
@@ -1,9 +0,0 @@
-import { env } from "@env";
-import { Elysia } from "elysia";
-
-export const HealthPlugin = new Elysia({ name: "health.plugin" })
-  .get("/health", () => ({
-    version: env.APP_VERSION,
-    service: env.APP_NAME,
-    status: "healthy"
-  }));
